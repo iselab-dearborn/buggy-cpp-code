365eca1f333db235d86264fb2837fe08794f70e2
Thiago Ferreira <thiagodnf@gmail.com>
2021-07-13 11:16:20 -0400

    test

--- run-sonarqube.bat
+++ run-sonarqube.bat
@@ -1,4 +1,9 @@
 
 build-wrapper-win-x86-64 --out-dir build_wrapper_output_directory compile.bat
 
-sonar-scanner.bat
+sonar-scanner.bat \
+  -D"sonar.organization=thiagodnf" \
+  -D"sonar.projectKey=thiagodnf_buggy-cpp-code" \
+  -D"sonar.sources=." \
+  -D"sonar.cfamily.build-wrapper-output=bw-output" \
+  -D"sonar.host.url=https://sonarcloud.io"
